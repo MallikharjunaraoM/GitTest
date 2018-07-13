4226e031cf49ba224f6ce9747c862fefa861a5c0
Mallik <mallikharjunarao.makkapati@gmail.com>
2018-07-14 00:24:17 +0530

    B completed

--- src/main/java/Cucumber/Automation/App.java
+++ src/main/java/Cucumber/Automation/App.java
@@ -11,5 +11,6 @@ public class App
         System.out.println( "Hello World!" );
         System.out.println( "Test2" );
         System.out.println( "Branch Testing" );
+        System.out.println( "Branch Testing completed" );
     }   
 }
