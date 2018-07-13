204607ca2ef1d288660bced603d32bb847314489
Mallik <mallikharjunarao.makkapati@gmail.com>
2018-07-14 00:14:29 +0530

    Test

--- src/main/java/Cucumber/Automation/App.java
+++ src/main/java/Cucumber/Automation/App.java
@@ -9,6 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        System.out.println( "Test2" );
+        System.out.println( "Test2" );        
     }   
 }
