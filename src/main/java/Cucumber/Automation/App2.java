22e451b6f975286a14e7702eacddf7675d162ef2
Mallik <mallikharjunarao.makkapati@gmail.com>
2018-07-14 12:35:56 +0530

    New from Git 1

--- src/main/java/Cucumber/Automation/App2.java
+++ src/main/java/Cucumber/Automation/App2.java
@@ -4,7 +4,7 @@ package Cucumber.Automation;
  * Hello world!
  *
  */
-public class App 
+public class App2 
 {
     public static void main( String[] args )
     {
---
+++ src/main/java/Cucumber/Automation/App4.java
@@ -0,0 +1,18 @@
+package Cucumber.Automation;
+
+/**
+ * Hello world!
+ *
+ */
+public class App4 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println( "Test2" );
+        System.out.println( "Branch Testing" );
+        System.out.println( "Branch Testing completed" );
+        System.out.println( "After git plugin" );
+
+    }   
+}
