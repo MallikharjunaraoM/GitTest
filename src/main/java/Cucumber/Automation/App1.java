e9ee8d841b71a530d9351c15c16ed78406a92341
Mallik <mallikharjunarao.makkapati@gmail.com>
2018-07-14 11:55:55 +0530

    New Pluging message

---
+++ src/main/java/Cucumber/Automation/App1.java
@@ -0,0 +1,14 @@
+package Cucumber.Automation;
+
+/**
+ * Hello world!
+ *
+ */
+public class App1 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "After git plugin latest" );
+
+    }   
+}
