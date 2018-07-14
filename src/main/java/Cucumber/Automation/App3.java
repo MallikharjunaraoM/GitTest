e20efd5e9dd94a9919f8e0db18ee5fe2a4c2a33d
Mallik <mallikharjunarao.makkapati@gmail.com>
2018-07-14 12:06:53 +0530

    from Git2

---
+++ src/main/java/Cucumber/Automation/App3.java
@@ -0,0 +1,17 @@
+package Cucumber.Automation;
+
+/**
+ * Hello world!
+ *
+ */
+public class App3 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println( "Test2" );
+        System.out.println( "Branch Testing" );
+        System.out.println( "Branch Testing completed" );
+
+    }   
+}
